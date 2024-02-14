9506d33293e4c490ab4ef0df19b9d6a5cb932f01
sherwin <sherace0311@gmail.com>
2024-02-14 16:18:48 +0800

    return a fullname with the value key

---
+++ Activity/activity_2.rb
@@ -0,0 +1,10 @@
+my_hash = {first_name: " sherwin", middle_name: " bonifacio", last_name: " bayaban"}
+
+
+def fullName(my_hash)
+  print "#{my_hash[:first_name]}"
+  print "#{my_hash[:middle_name,]}"
+  print "#{my_hash[:last_name]}"
+end
+
+print fullName(my_hash)
